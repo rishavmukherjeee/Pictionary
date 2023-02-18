58ae066edfab6390efe68671e116f06c8cf22902
Rishav Mukherjee <rishavmukherjee11@gmail.com>
2023-02-18 10:26:09 +0530

    Paint

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { Svg, Path } from 'react-native-svg';
 
-export default function App() {
+export default function PaintApp() {
+  const [path, setPath] = useState([]);
+
+  const handleTouch = (event) => {
+    const locationX = event.nativeEvent.locationX;
+    const locationY = event.nativeEvent.locationY;
+    setPath([...path, { x: locationX, y: locationY }]);
+  };
+
+  const handleClear = () => {
+    setPath([]);
+  };
+
+  const canvas = useRef(null);
+
+  const renderPath = () => {
+    if (!path || path.length === 0) {
+      return null;
+    }
+
+    const pathData = path.map((point, index) => {
+      if (index === 0) {
+        return `M ${point.x} ${point.y}`;
+      } else {
+        return `L ${point.x} ${point.y}`;
+      }
+    }).join(' ');
+
+    return (
+      <View style={styles.canvasWrapper}>
+        <Svg width="100%" height="100%">
+          <Path d={pathData} stroke="black" strokeWidth="3" fill="none" />
+        </Svg>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.canvasWrapper} ref={canvas}>
+        {renderPath()}
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={handleClear}>
+          <Text style={styles.buttonText}>Clear</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.invisibleLayer} onTouchStart={handleTouch} onTouchMove={handleTouch} />
     </View>
   );
 }
@@ -16,5 +60,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  canvasWrapper: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#eee',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: '#333',
+    borderRadius: 5,
+    marginRight: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  invisibleLayer: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
   },
 });
